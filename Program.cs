e5f1549c7b715ff2fdcce406dda897b1fdfdc87e
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-13 16:29:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace AppCalcularTriangulo_XY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            string[] vetX = Console.ReadLine().Split(" ");
+            double XA = double.Parse(vetX[0]);
+            double XB = double.Parse(vetX[1]);
+            double XC = double.Parse(vetX[2]);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            string[] vetY = Console.ReadLine().Split(" ");
+            double YA = double.Parse(vetY[0]);
+            double YB = double.Parse(vetY[1]);
+            double YC = double.Parse(vetY[2]);
+
+            Triangulo triangulo = new Triangulo(XA, XB, XC, YA, YB, YC);
+            double triangulox = triangulo.calcularValorTrianguloX();
+            double trianguloY = triangulo.calcularValorTrianguloY();
+            string maiorArea = triangulo.maiorArea();
+
+            Console.Clear();
+            Console.WriteLine("Abaixo resultados entre X e Y: ");
+            Console.WriteLine("Area de X = {0}", triangulox.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y = {0}", trianguloY.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Maior area = {0}", maiorArea);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCalcularTriangulo_XY
+{
+    class Triangulo
+    {
+        private double xa;
+        private double xb;
+        private double xc;
+
+        private double yA;
+        private double yb;
+        private double yc;
+
+        #region Construtor
+
+
+        public Triangulo(double XA, double XB, double XC,double YA, double YB, double YC)
+        {
+            //Triângulo X
+            this.xa = XA;
+            this.xb = XB;
+            this.xc = XC;
+
+            //Triângulo Y
+            this.yA = YA;
+            this.yb = YB;
+            this.yc = YC;
+        }
+
+        #endregion
+
+        #region Métodos
+
+        private double calculaValorP(double A, double B, double C)
+        {
+            double p = (A + B + C) / 2;
+            return p;
+        }
+
+        public double calcularValorTrianguloX()
+        {
+            double p = calculaValorP(xa, xb, xc);
+            double area = Math.Sqrt(p * (p - xa) * (p - xb) * (p - xc));
+            return area;
+        }
+
+        public double calcularValorTrianguloY()
+        {
+            double p = calculaValorP(yA, yb, yc);
+            double area = Math.Sqrt(p * (p - yA) * (p - yb) * (p - yc));
+            return area;
+        }
+
+        public string maiorArea() 
+        {
+            double areax = calcularValorTrianguloX();
+            double areay = calcularValorTrianguloY();
+            string msg = string.Empty;
+
+            if (areax > areay)
+            {
+                return msg = "X";
+            }
+            else
+            {
+                return msg = "Y";
+            }
+        }
+
+        #endregion
+    }
+}
